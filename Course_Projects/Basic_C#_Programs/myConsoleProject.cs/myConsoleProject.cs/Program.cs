d91bab3874d7870944f0a27578a63f50cd45c9c5
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-04-28 19:56:31 -0700

    Challenge Double Quotes

--- Course_Projects/Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ Course_Projects/Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -6,7 +6,7 @@ namespace myConsoleProject.cs
     {
         static void Main()
         {
-            Console.WriteLine("What is your name?");
+            Console.WriteLine("What\" \" is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Hello, " + name + "!");
             Console.Read();
