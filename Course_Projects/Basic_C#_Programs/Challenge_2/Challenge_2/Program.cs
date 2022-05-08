4ba55f68d0944ae4371d7c9066b44f2d88372588
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-08 12:57:11 -0700

    Challenge 2

---
+++ Course_Projects/Basic_C#_Programs/Challenge_2/Challenge_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Challenge_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string stringConcant1 = "Yo-";
+            string stringConcant2 = "Dawgus";
+            Console.WriteLine(stringConcant1 + stringConcant2);
+            Console.ReadLine();
+        }
+    }
+}
