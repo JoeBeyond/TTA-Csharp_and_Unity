af65a0493d608f36462cf76f3a5e53e6384f730d
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-07 19:25:53 -0700

    Challenge 1

---
+++ Course_Projects/Basic_C#_Programs/Challenge_1/Challenge_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Challenge_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = 1;
+            int num2 = 2;
+            int num3 = 3;
+
+            Console.WriteLine("Addition of 1 and 2.");
+            int sum1 = num1 + num2;
+            Console.WriteLine(sum1);
+            Console.ReadLine();
+
+            Console.WriteLine("Subtraction of 1 and 3.");
+            int diff1 = num1 - num3;
+            Console.WriteLine(diff1);
+            Console.ReadLine();
+
+            Console.WriteLine("Multiplication of 2 and 2.");
+            int product1 = num2 * num2;
+            Console.WriteLine(product1);
+            Console.ReadLine();
+
+            Console.WriteLine("Division of 2 and 1.");
+            int dividend1 = num2 / num1;
+            Console.WriteLine(dividend1);
+            Console.ReadLine();
+        }
+    }
+}
