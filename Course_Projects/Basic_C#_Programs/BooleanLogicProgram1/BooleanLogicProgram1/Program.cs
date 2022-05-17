94750abcc556fdf3feaf59435f11b1167868edb7
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-16 22:51:37 -0700

    Boolean Operators Video

---
+++ Course_Projects/Basic_C#_Programs/BooleanLogicProgram1/BooleanLogicProgram1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicProgram1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AND operator
+
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+            Console.ReadLine();
+
+            //OR operator
+
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+            Console.ReadLine();
+
+            //DOES operator
+
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+            Console.ReadLine();
+
+            //DOES NOT operator
+
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+            Console.ReadLine();
+
+            //X-OR operator
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+            Console.ReadLine();
+
+        }
+    }
+}
