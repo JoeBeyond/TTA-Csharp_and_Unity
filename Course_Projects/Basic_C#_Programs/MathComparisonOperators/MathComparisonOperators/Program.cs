3afd8be68f8aba40db01a1323819b166c5d4ed2b
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-17 13:47:39 -0700

    Anon Income Comparison Program v2

--- Course_Projects/Basic_C#_Programs/MathComparisonOperators/MathComparisonOperators/Program.cs
+++ Course_Projects/Basic_C#_Programs/MathComparisonOperators/MathComparisonOperators/Program.cs
@@ -14,39 +14,38 @@ namespace MathComparisonOperators
             Console.WriteLine("PRESS ENTER");
             Console.ReadLine();
 
-            int hourRate1 = 15;
-            int hourRate2 = 20;
-            int hourWeekA = 40;
-            int salary1 = 32000;
-            int salary2 = 41600;
 
-            Console.WriteLine("Person 1");
-            Console.ReadLine();
+            Console.WriteLine("Person 1: input rate of pay as an integer.");
+            int hourRate1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hourly Rate: " + hourRate1);
-            Console.ReadLine();
+            Console.WriteLine("Person 1: input hours per week as an integer.");
+            int hourWeekA = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hours Worked per Week: " + hourWeekA);
-            Console.ReadLine();
 
-            Console.WriteLine("Person 2");
-            Console.ReadLine();
+
+            Console.WriteLine("Person 2: input rate of pay as an integer.");
+            int hourRate2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hourly Rate: " + hourRate2);
-            Console.ReadLine();
-            Console.WriteLine("Hours Worked per Week: " + hourWeekA);
+            Console.WriteLine("Person 2: input hours per week as an integer.");
+            int hourWeekB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours Worked per Week: " + hourWeekB);
             Console.ReadLine();
 
-            
+
+            int salary1 = (hourRate1 * hourWeekA * 52);
             Console.WriteLine("Person 1 Salary: " + salary1);
             Console.ReadLine();
+
+            int salary2 = (hourRate2 * hourWeekB * 52);
             Console.WriteLine("Person 2 Salary: " + salary2);
             Console.ReadLine();
 
-            Console.WriteLine("Does person 1 make more money than person 2?");
+            Console.WriteLine("Does person 1 make more money than person 2? Press ENTER.");
             Console.ReadLine();
             Console.WriteLine(salary1 > salary2);
             Console.ReadLine();
             Console.WriteLine("END");
             Console.ReadLine();
-            // test
         }
     }
 }
