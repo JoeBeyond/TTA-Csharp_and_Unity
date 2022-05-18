88b5c9da8fa1cf95610f98dd26fc2362099c0885
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-17 22:12:16 -0700

    Console Output Challenge
    <, > operators

---
+++ Course_Projects/Basic_C#_Programs/ConsoleOutputChallenge/ConsoleOutputChallenge/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOutputChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greater Than Operation
+
+            Console.WriteLine("This is a Greater Than and Lesser Than test.");
+            Console.WriteLine("Input an integer please.");
+            string num1 = Console.ReadLine();
+            int num2 = Convert.ToInt32(num1);
+            Console.WriteLine("First number is: " + num2);
+
+
+            Console.WriteLine("Input an integer please.");
+            string num3 = Console.ReadLine();
+            int num4 = Convert.ToInt32(num3);
+            Console.WriteLine("Second number is: " + num4);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Is the first number GREATER THAN the second?");
+            Console.WriteLine(num2 > num4);
+
+            // Lesser Than Operation
+
+            Console.WriteLine("Input an integer please.");
+            string num5 = Console.ReadLine();
+            int num6 = Convert.ToInt32(num5);
+            Console.WriteLine("First number is: " + num6);
+
+
+            Console.WriteLine("Input an integer please.");
+            string num7 = Console.ReadLine();
+            int num8 = Convert.ToInt32(num7);
+            Console.WriteLine("Second number is: " + num8);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Is the first number LESSER THAN the second?");
+            Console.WriteLine(num6 < num8);
+            Console.WriteLine("END OF TEST: Please Press ENTER to exit program.");
+            Console.ReadLine();
+        }
+    }
+}
