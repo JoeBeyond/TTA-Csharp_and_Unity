969776466ca959b9b6cb6da706cd347578d30ea7
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-17 17:43:10 -0700

    Conditional Statements Challenge

---
+++ Course_Projects/Basic_C#_Programs/ConditionalStatementsChallenge/ConditionalStatementsChallenge/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatementsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer between 0 and 5. Or not..");
+
+
+            string num1 = Console.ReadLine();
+            int num2 = Convert.ToInt32(num1);
+
+
+            if (num2 <= 2)
+            {
+                num2++;
+                Console.WriteLine(".IF. " + num2);
+            }
+            else if (num2 <= 5)
+            {
+                num2--;
+                Console.WriteLine(".ELSE IF. " + num2);
+            }
+            else
+            {
+                Console.WriteLine(".ELSE. " + num2);
+            }
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Course_Projects/Basic_C#_Programs/ConditionalStatementsChallenge/ConditionalStatementsChallenge/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConditionalStatementsChallenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConditionalStatementsChallenge")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e913b07-e8c8-4540-b2cc-084644c471e0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
