7164e30207ff0210faedb47a7ccdd34ed4719643
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-20 20:11:15 -0700

    Strings Challenge

---
+++ Course_Projects/Basic_C#_Programs/StringsChallenge/StringsChallenge/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //User input for strings
+
+            Console.WriteLine("Input a string.");
+            string str1 = Console.ReadLine();
+
+            Console.WriteLine("Input second string.");
+            string str2 = Console.ReadLine();
+
+            Console.WriteLine("Input third string.");
+            string str3 = Console.ReadLine();
+
+            // String concatenation
+
+            string concat = str1 + str2 + str3;
+            Console.WriteLine(concat);
+
+            concat = concat.ToUpper();
+            Console.WriteLine(concat);
+
+            // StringBuilders forming paragraph
+
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append("Don't forget to ration your coffee.");
+            Console.WriteLine(sb1);
+
+            StringBuilder sb2 = new StringBuilder();
+            sb2.Append("A long time ago there were these creatures that tended the plants for us...");
+            Console.WriteLine(sb2);
+
+            StringBuilder sb3 = new StringBuilder();
+            sb3.Append("We've taken the mantle of their labor to sustain life. So appreciate that cup of Joe.");
+            Console.WriteLine(sb3);
+
+            Console.WriteLine("END");
+            Console.ReadLine();
+        }
+    }
+}
