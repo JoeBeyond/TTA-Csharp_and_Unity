1f0a9258ebc8b469a20432f68c838136df64f04e
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-27 13:07:29 -0700

    Exception Handling

--- Course_Projects/Basic_C#_Programs/ExceptionHandling/ExceptionHandling/Program.cs
+++ Course_Projects/Basic_C#_Programs/ExceptionHandling/ExceptionHandling/Program.cs
@@ -10,6 +10,41 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick another number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the numbers...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            catch (DivideByZeroException Zero)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+                Console.WriteLine(Zero.Message);
+            }
+
+            catch (Exception ex1)
+            {
+                Console.WriteLine("General Exception");
+            }
+
+            finally
+            {
+                Console.WriteLine("END preogram");
+                Console.ReadLine();
+            }
         }
     }
 }
