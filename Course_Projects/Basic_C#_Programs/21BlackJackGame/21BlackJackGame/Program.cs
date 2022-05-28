0ec0ce8b3b1bd2a68185410102b1db309bdac89a
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-27 20:31:21 -0700

    21 Black Jack Game

--- Course_Projects/Basic_C#_Programs/21BlackJackGame/21BlackJackGame/Program.cs
+++ Course_Projects/Basic_C#_Programs/21BlackJackGame/21BlackJackGame/Program.cs
@@ -11,11 +11,15 @@ namespace _21BlackJackGame
         static void Main(string[] args)
         {
             Deck deck = new Deck();
+            deck = Shuffle(deck);
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
+
+            
+
 
             //deck.Cards = new List<Card>();
 
@@ -27,9 +31,28 @@ namespace _21BlackJackGame
 
             //Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
 
+
+
+
+
             Console.WriteLine(deck.Cards.Count);
 
             Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = TempList;
+            return deck;
         }
     }
 }
