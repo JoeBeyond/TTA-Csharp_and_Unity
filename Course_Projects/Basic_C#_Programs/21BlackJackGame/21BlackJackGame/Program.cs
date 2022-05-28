869161264e53065e62d132f47902db8194d55c15
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-05-27 19:48:18 -0700

    Black Jack 21 Game
    I did this earlier just forgot to turn in.

---
+++ Course_Projects/Basic_C#_Programs/21BlackJackGame/21BlackJackGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21BlackJackGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+
+            //deck.Cards = new List<Card>();
+
+            //Card cardOne = new Card();
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+
+            //deck.Cards.Add(cardOne);
+
+            //Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
+
+            Console.WriteLine(deck.Cards.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
