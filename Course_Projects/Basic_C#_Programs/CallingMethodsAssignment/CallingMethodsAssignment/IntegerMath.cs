68a680648a07f21190f1d8c1a0ce4da0cfdf9c76
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-06 21:48:12 -0700

    Calling Methods Assignment

---
+++ Course_Projects/Basic_C#_Programs/CallingMethodsAssignment/CallingMethodsAssignment/IntegerMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    // Class below; the subprogram
+    public class IntegerMath
+    {
+        // Function below; of the class
+
+        public int AddNumber(int x)
+        {
+            int sum = x + 28;
+            return sum;
+        }
+
+        public int SubtractNumber(int x)
+        {
+            int difference = x - 28;
+            return difference;
+        }
+
+        public int MultiplyNumber(int x)
+        {
+            int product = x * 28;
+            return product;
+        }
+    }
+}
---
+++ Course_Projects/Basic_C#_Programs/CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Initialize a sub program with its own routine. Able to call the routine as needed. The methods in Program.cs file apply to []Main.
+
+            IntegerMath obj1 = new IntegerMath();
+
+            Console.WriteLine("Please Input a mnumber.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int result1 = obj1.AddNumber(num1);
+            Console.WriteLine(result1);
+            int result2 = obj1.SubtractNumber(num1);
+            Console.WriteLine(result2);
+            int result3 = obj1.MultiplyNumber(num1);
+            Console.WriteLine(result3);
+
+            Console.ReadLine();
+        }
+    }
+}
