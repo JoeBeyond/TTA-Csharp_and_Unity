4180869995e557fe50ce7f8504760dee85cde45a
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-09 00:52:29 -0700

    Main Method Assignment

---
+++ Course_Projects/Basic_C#_Programs/MainMethod/MainMethod/DisplayResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class DisplayResult
+    {
+        public int Integer(int a3)
+        {
+            int math1 = a3 * 111 / 32 + 9472 - 685;
+            return math1;
+        }
+
+        public decimal Decimal(decimal b5)
+        {
+            decimal decimal1 = b5 + 182.387m * 23.374m;
+            Math.Round(decimal1);
+            return decimal1;
+        }
+
+        public string String(string c7)
+        {
+            string string1 = c7;
+
+            try
+            {
+                //string string1 = c7;
+
+                int dataTypeConversion = Convert.ToInt32(c7);
+
+                Console.WriteLine(dataTypeConversion);
+            }
+            catch (System.FormatException xy)
+            {
+                Console.WriteLine(xy.Message + " , terminating program.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            return string1;
+        }
+    }
+}
---
+++ Course_Projects/Basic_C#_Programs/MainMethod/MainMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DisplayResult data = new DisplayResult();
+
+            Console.WriteLine("Please input an integer.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int result1 = data.Integer(num1);
+            Console.WriteLine(result1);
+
+            Console.WriteLine("Please input a decimal.");
+            decimal dec1 = Convert.ToDecimal(Console.ReadLine());
+            decimal result2 = data.Decimal(dec1);
+            Console.WriteLine(result2);
+
+            Console.WriteLine("Please input a string.");
+            string str1 = Convert.ToString(Console.ReadLine());
+            string result3 = data.String(str1);
+            Console.WriteLine(result3);
+
+            Console.WriteLine("END PROGRAM");
+            Console.ReadLine();
+        }
+    }
+}
