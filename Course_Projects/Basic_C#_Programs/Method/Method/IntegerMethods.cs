8006627823d8cc9cebc89e04c52652f9c7902027
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-09 15:30:08 -0700

    Method Assignment

---
+++ Course_Projects/Basic_C#_Programs/Method/Method/IntegerMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class IntegerMethods
+    {
+        public int Math1(int a, int b = 30)
+        {
+            int comboInt = a * b;
+            return comboInt;
+        }
+    }
+}
---
+++ Course_Projects/Basic_C#_Programs/Method/Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IntegerMethods calculations = new IntegerMethods();
+
+            Console.WriteLine("Input an integer.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You have a choice to input another integer or not.");
+
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int outputB = calculations.Math1(num1, num2);
+                Console.WriteLine(outputB);
+            }
+            catch
+            {
+                int outputA = calculations.Math1(num1);
+                Console.WriteLine(outputA);
+            }
+
+            Console.WriteLine("END PROGRAM");
+            Console.ReadLine();
+        }
+    }
+}
