65df70d83045440dc9b9d07d1f0946fc6c57c23c
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-15 18:22:50 -0700

    Method Class

---
+++ Course_Projects/Basic_C#_Programs/MethodClass/MethodClass/IntegerOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class IntegerOutput
+    {
+        public void ParameterOperation(int a, int b)
+        {   
+            int math1 = a + 10 * 10 - 35;
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Course_Projects/Basic_C#_Programs/MethodClass/MethodClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IntegerOutput output = new IntegerOutput();
+
+            output.ParameterOperation(3, 5);
+
+            output.ParameterOperation(b: 33, a: 55);
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
