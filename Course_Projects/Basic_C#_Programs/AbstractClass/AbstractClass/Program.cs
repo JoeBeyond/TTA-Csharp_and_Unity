ddc383b09248f7d0e32d3b81d061f14aa7238356
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-17 19:39:35 -0700

    Abstract Class

---
+++ Course_Projects/Basic_C#_Programs/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee individual = new Employee();
+            individual.FirstName = "Sample";
+            individual.LastName = "Student";
+            individual.SayName();
+            Console.ReadLine();
+        }
+    }
+}
