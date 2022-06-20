e11702af81ddcaf7e6870153c81d4196de887727
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-20 13:11:24 -0700

    Polymorphism

---
+++ Course_Projects/Basic_C#_Programs/Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("I QUIT! I'm taking the doughnuts.");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
