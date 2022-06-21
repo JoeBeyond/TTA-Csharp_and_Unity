00ed9a16dc072f64528241a2cc5e926cebd834e3
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-20 18:07:46 -0700

    Overloaded Operator

---
+++ Course_Projects/Basic_C#_Programs/OperatorsOverloaded/OperatorsOverloaded/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloaded
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public static bool operator== (Employee obj1, Employee obj2)
+        {
+            return obj1.ID == obj2.ID;
+        }
+        public static bool operator!= (Employee obj1, Employee obj2)
+        {
+            return obj1.ID != obj2.ID;
+        }
+    }
+}
---
+++ Course_Projects/Basic_C#_Programs/OperatorsOverloaded/OperatorsOverloaded/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloaded
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee obj1 = new Employee();
+            Employee obj2 = new Employee();
+            obj1.ID = 0;
+            obj2.ID = 0;
+            Console.WriteLine(obj1.ID == obj2.ID);
+            Console.ReadLine();
+            
+        }
+    }
+}
