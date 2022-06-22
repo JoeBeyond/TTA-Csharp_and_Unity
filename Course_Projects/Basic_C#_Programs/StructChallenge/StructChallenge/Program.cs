dd68b20f0a9a47d95922c5803ef73ce53225673f
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-22 02:26:48 -0700

    Struct Challenge

---
+++ Course_Projects/Basic_C#_Programs/StructChallenge/StructChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number100 = new Number();
+            number100.Amount = 100;
+            Console.WriteLine(number100);
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+        
+    }
+}
