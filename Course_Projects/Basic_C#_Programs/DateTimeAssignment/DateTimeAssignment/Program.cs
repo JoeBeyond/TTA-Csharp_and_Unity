e11c5ec661b0b22f9e0146a07f3afa41efdd80c1
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-27 05:26:57 -0700

    DateTime Assignment

---
+++ Course_Projects/Basic_C#_Programs/DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2022, 06, 27, 4, 52, 30);
+
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Enter an integer.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(DateTime.Now.AddHours(num1));
+
+            Console.WriteLine("END PROGRAM");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
