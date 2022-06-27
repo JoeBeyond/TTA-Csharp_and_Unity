92e10e71dd43a3bd153ebffbed986ec3688ac7fd
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-06-27 04:15:00 -0700

    Input Challenge

---
+++ Course_Projects/Basic_C#_Programs/InputChallenge/InputChallenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input an integer.");
+            string num1 = Console.ReadLine();
+
+            File.WriteAllText(@"C:\ThisPC\Documents\textfile.txt", num1);
+
+            using (StreamWriter file = new StreamWriter(@"C:\ThisPC\Documents\textfile.txt", true))
+            {
+                file.WriteLine(num1);
+            }
+
+            Console.WriteLine("END PROGRAM");
+            Console.ReadLine();
+        }
+    }
+}
